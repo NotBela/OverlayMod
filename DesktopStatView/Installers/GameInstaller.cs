1a3606b0c02cda2e014b52cac4b135c1aded93b3
Bela <27pvarga@gmail.com>
2024-05-31 12:17:27 -0400

    Add project files.

---
+++ DesktopStatView/Installers/GameInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace DesktopStatView.Installers
+{
+    internal class GameInstaller : Installer<GameInstaller>
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<StatController>().AsSingle();
+        }
+    }
+}
